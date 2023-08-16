eec8e372345073cd6a344320939b519102b0c5c9
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-08-16 10:44:18 +0530

    Time: 365 ms (29.63%) | Memory: 10.1 MB (74.35%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int>v;
+        int flag=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    flag=1;
+                    v.push_back(i);
+                    v.push_back(j);
+                    break;
+                }
+            }
+            if(flag==1){
+                break;
+            }
+        }
+        return v;
+    }
+};
