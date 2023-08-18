5a91e9befe23b1676d54dc66a731f05f34dd8e9b
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-08-18 18:57:12 +0530

    Time: 7 ms (95.35%) | Memory: 15.8 MB (9.37%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int>v;
+        int n=numbers.size();
+        int i=0,j=n-1;
+        while(i<j){
+            if(numbers[i]+numbers[j]>target){
+              j--;  
+            }
+            else if(numbers[i]+numbers[j]<target){
+                i++;
+            }
+            else if(numbers[i]+numbers[j]==target){
+                v.push_back(i+1);
+                v.push_back(j+1);
+                break;
+            }
+        }
+        return v;
+    }
+};
