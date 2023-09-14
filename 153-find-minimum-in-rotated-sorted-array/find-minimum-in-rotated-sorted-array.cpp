fb852b555fbb1805d937f7d6f81c4ee8a5af60cd
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-09-14 19:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (12.04%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        //int min=99999999999;
-        // for(int i=0;i<nums.size();i++){
-        //     //************** THE FIRST APPROACH *******************
-        //     //
-        //     // if(min>nums[i]){
-        //     //     min=nums[i];
-        //     // }
+        int min=99999999999;
+        for(int i=0;i<nums.size();i++){
+            //************** THE FIRST APPROACH *******************
+            
+            if(min>nums[i]){
+                min=nums[i];
+            }
             
             
 
-        // }
+        }
+        return min;
         //************** THE SECOND APPROACH *******************
-        sort(nums.begin(),nums.end());
+        //beats 100% users runtime and 12.04% in memory
+        // sort(nums.begin(),nums.end());
         
-        return nums[0];
+        // return nums[0];
     }
 };
