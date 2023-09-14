b0e97073068cdd6fe28b35b03663b490ecda04c1
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-09-15 00:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (20.57%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int i;
-        for(i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                break;
+        //****************FIRST APPROACH****************
+        // int i;
+        // for(i=0;i<nums.size();i++){
+        //     if(nums[i]==target){
+        //         break;
+        //     }
+        // }
+        // if(nums.size()==i){
+        //     return -1;
+        // }
+        // return i;
+        //****************SECOND APPROACH****************
+        int n=nums.size();
+        int left=0;
+        int right=nums.size()-1;
+        int mid=(right+left)/2;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>=nums[left]){
+                if(target >= nums[left] && target <=nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+            else{
+                if(target>=nums[mid]&& target<=nums[right]){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
             }
         }
-        if(nums.size()==i){
-            return -1;
-        }
-        return i;
+        return -1;
+        
     }
 };
