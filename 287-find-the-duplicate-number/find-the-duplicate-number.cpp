cfe6a7b7e4466e67dfa086fdeaa51c117676ccfd
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-09-16 02:22:23 +0530

    Time: 219 ms (8.91%) | Memory: 101.1 MB (6.64%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        //*************[ BRUTE FORCE APPROACH ]*************
         int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        //*************[ HASH TABLE APPROACH ]*************
+        
+        unordered_map<int,int>m;
+        for(int i=0;i<n;i++){
+            m[nums[i]]++;
+        }
+        for(auto p:m){
+            if(p.second>1){
+                return p.first;
             }
         }
         return -1;
+
     }
 };
