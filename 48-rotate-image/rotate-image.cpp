d3ab58798ee92b9e07336947c9511d3611fe7d32
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-09-15 19:39:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (21.80%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>>ans(n, vector<int>(n));
+        //*******************FIRST SOLUTION [ BRUTE FORCE ]*******************
+        // vector<vector<int>>ans(n, vector<int>(n));
         
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         ans[j][n-i - 1]=matrix[i][j];
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<ans[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         matrix[i][j]=ans[i][j];
+        //     }
+        // }
+        //*******************SECOND SOLUTION [ TRANSPOSE AND REVERSE ]*******************
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans[j][n-i - 1]=matrix[i][j];
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+
             }
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cout<<ans[i][j]<<" ";
-            }
-            cout<<endl;
+            reverse(matrix[i].begin(),matrix[i].end());
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=ans[i][j];
-            }
-        }
-        
     }
 };
