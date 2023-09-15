d062f55813b187f9036260cb3817980943c1a75d
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-09-15 18:14:37 +0530

    Time: 4 ms (51.96%) | Memory: 12.5 MB (26.57%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-
+        //next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+        //int bpno;
+        int idx=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                //pno=nums[i];
+                break;
+            }
+        }
+        if(idx==-1){
+            reverse(nums.begin(),nums.end());
+            return ;
+        }
+        for(int i=n-1;i>=idx;i--){
+            if(nums[idx]<nums[i]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+        reverse(nums.begin()+idx+1,nums.end());
+       return;
     }
 };
