fb58fa472319fd3feea571803ee885ac3a995924
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-09-16 13:36:27 +0530

    Time: 14 ms (65.84%) | Memory: 19.8 MB (10.57%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         //**************** [ HASH MAP APPROACH ] ****************
-        
-        unordered_map<int,int>m;
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
-        }
-        for(auto ite:m){
-            if(ite.second>(nums.size()/2)){
-                return ite.first;
-            }
-        }
-        return -1;
+        // unordered_map<int,int>m;
+        // for(int i=0;i<nums.size();i++){
+        //     m[nums[i]]++;
+        // }
+        // for(auto ite:m){
+        //     if(ite.second>(nums.size()/2)){
+        //         return ite.first;
+        //     }
+        // }
+        // return -1;
+        //**************** [ SOLUTION BASED APPROACH ] ****************
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+
     }
 };
