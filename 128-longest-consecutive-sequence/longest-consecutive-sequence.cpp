8f3d745b1d0d0a35b2a5594d666fdbb71aa75040
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-10-02 00:38:30 +0530

    Time: 65 ms (91.24%) | Memory: 45.9 MB (74.30%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0){
+        if(nums.empty()){
             return 0;
         }
         sort(nums.begin(),nums.end());
-        int maxcnt=1;
+        int max=1;
         int cnt=1;
-
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==(nums[i-1]+1)){
-                cnt++;
-                
+        for(int i=0;i<(nums.size()-1);i++){
+            
+            if(nums[i+1]==(nums[i]+1)){
+                cnt++;  
             }
-            else if(nums[i] != nums[i - 1]){
-                 maxcnt = max(maxcnt, cnt);
-                 cnt = 1;
+            else if (nums[i] != nums[i+1]) {
+                cnt = 1;
+            }
+            if(max<cnt){
+                max=cnt;
             }
         }
-        
-        return max(maxcnt,cnt);
+        return max;
     }
 };
