1ef459a26ca477f194dafd09eae32345a586523f
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2023-12-26 12:45:55 +0530

    Time: 9 ms (47.05%) | Memory: 11 MB (12.04%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,17 +9,29 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *f=head;
-        ListNode *s=head;
-        if(f==NULL || f->next==NULL){
-            return false;
-        }
-        while(f && f->next!=NULL){
-            f=f->next->next;
-            s=s->next;
-            if(f==s){
+        //**********[ FAST AND SLOW POINTER METHOD ]**********
+        // ListNode *f=head;
+        // ListNode *s=head;
+        // if(f==NULL || f->next==NULL){
+        //     return false;
+        // }
+        // while(f && f->next!=NULL){
+        //     f=f->next->next;
+        //     s=s->next;
+        //     if(f==s){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        //************[ SET METHOD ]*************
+        ListNode *temp=head;
+        unordered_set<ListNode*> vnode;
+        while(temp!=NULL){
+            if(vnode.find(temp)!=vnode.end()){
                 return true;
             }
+            vnode.insert(temp);
+            temp=temp->next;
         }
         return false;
     }
