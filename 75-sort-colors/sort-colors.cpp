bbbc52dd9f2faaf3e7c0fd8f239cdb24524ccb0c
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-13 13:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.85%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
+        //sort(nums.begin(),nums.end());
+        int z,o,t;
+        z=o=t=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                z++;
+            }
+            else if(nums[i]==1){
+                o++;
+            }
+            else{
+                t++;
+            }
+
+        }
+        cout<<z<<" "<<o<<" "<<t;
+        for(int i=0;i<nums.size();i++){
+            if(z!=0){
+                nums[i]=0;
+                z=z-1;
+            }
+            else if(z==0 && o!=0){
+                nums[i]=1;
+                o=o-1;
+            }
+            else if(z==0 && o==0 && t!=0){
+                nums[i]=2;
+                t=t-1;
+            }
+        }
     }
 };
