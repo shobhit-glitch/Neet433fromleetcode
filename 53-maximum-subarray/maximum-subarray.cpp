6691bab475ec820475aca38894a4183df8473fe9
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-13 12:07:24 +0530

    Time: 85 ms (20.36%) | Memory: 70.1 MB (92.63%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum=0;
-        int max=-99999;
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+        long long max=-99999;
+        long long  sum=0;
+        for(int i=0;i<n;i++){
             sum+=nums[i];
-            cout<<sum<<' ';
             if(sum>max){
                 max=sum;
             }
             if(sum<0){
                 sum=0;
+
             }
         }
         return max;
-
     }
 };
