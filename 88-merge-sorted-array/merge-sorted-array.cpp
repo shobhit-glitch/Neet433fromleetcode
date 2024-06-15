68f20d42712c8c7fd5079ec3ae6d11dfceb801f8
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-15 11:44:47 +0530

    Time: 5 ms (28.01%) | Memory: 11 MB (71.64%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -8,29 +8,44 @@ public:
        
     //    sort(nums1.begin(),nums1.end());
     // SWAP METHOD
-    int i=m-1;
+    // int i=m-1;
     
-    int j=0;
+    // int j=0;
     
 
-    while(i>=0 && j<n){
-        if(nums1[i]>nums2[j]){
-            swap(nums1[i],nums2[j]);
-            cout<<"swapped nums1 :"<<nums1[i];
-            i--;j++;
+    // while(i>=0 && j<n){
+    //     if(nums1[i]>nums2[j]){
+    //         swap(nums1[i],nums2[j]);
+    //         cout<<"swapped nums1 :"<<nums1[i];
+    //         i--;j++;
+    //     }
+    //     else{
+    //         break;
+    //     }
+        
+    // }
+    // for(int k=0;k<nums1.size();k++){
+    //     cout<<nums1[k]<<endl;
+    // }
+    // for(int i=0;i<n;i++){
+    //     nums1[m+i]=nums2[i];
+    // }
+    // sort(nums1.begin(),nums1.end());
+    //2 pointer 
+    int i=m-1;
+    int j=n-1;
+    int k=m+n-1;
+    while(j>=0){
+        if(i>=0 && nums1[i]>nums2[j]){
+            nums1[k]=nums1[i];
+            k--;i--;
+
         }
         else{
-            break;
+            nums1[k]=nums2[j];
+            k--;j--;
         }
-        
     }
-    for(int k=0;k<nums1.size();k++){
-        cout<<nums1[k]<<endl;
-    }
-    for(int i=0;i<n;i++){
-        nums1[m+i]=nums2[i];
-    }
-    sort(nums1.begin(),nums1.end());
       
     
     }
