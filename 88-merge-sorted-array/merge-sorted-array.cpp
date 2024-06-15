634f971dc1d0fed80577c751175d6458f86f180d
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-15 11:06:27 +0530

    Time: 8 ms (7.85%) | Memory: 10.9 MB (71.64%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -2,11 +2,36 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
        // THE BASIC APPROACH
-       for(int i=0;i<n;i++){
-        nums1[m+i]=(nums2[i]);
-       }
+    //    for(int i=0;i<n;i++){
+    //     nums1[m+i]=(nums2[i]);
+    //    }
        
-       sort(nums1.begin(),nums1.end());
+    //    sort(nums1.begin(),nums1.end());
+    // SWAP METHOD
+    int i=m-1;
+    
+    int j=0;
+    
+
+    while(i>=0 && j<n){
+        if(nums1[i]>nums2[j]){
+            swap(nums1[i],nums2[j]);
+            cout<<"swapped nums1 :"<<nums1[i];
+            i--;j++;
+        }
+        else{
+            break;
+        }
         
+    }
+    for(int k=0;k<nums1.size();k++){
+        cout<<nums1[k]<<endl;
+    }
+    for(int i=0;i<n;i++){
+        nums1[m+i]=nums2[i];
+    }
+    sort(nums1.begin(),nums1.end());
+      
+    
     }
 };
