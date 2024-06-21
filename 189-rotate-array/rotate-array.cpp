37ecc2d8de18a8bddd5018107d4e1a0a1e1a0fb2
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-22 00:50:12 +0530

    Time: 25 ms (24.69%) | Memory: 27.3 MB (55.56%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -16,15 +16,9 @@ public:
         //     nums[i]=sec[i];
         // }
         int n = nums.size();
-        k = k % n; // Ensure k is within the range [0, n)
-
-        // Reverse the entire array
+        k = k % n; 
         reverse(nums.begin(), nums.end());
-        
-        // Reverse the first k elements
         reverse(nums.begin(), nums.begin() + k);
-        
-        // Reverse the rest of the elements after k
         reverse(nums.begin() + k, nums.end());
         
     }
