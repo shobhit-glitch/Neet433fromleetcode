1ad7f033978044d86e878711fb0cc4d080984f55
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-22 00:49:34 +0530

    Time: 16 ms (86.76%) | Memory: 27.3 MB (55.56%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,18 +3,29 @@ public:
 
     void rotate(vector<int>& nums, int k) {
         
-        int n=nums.size();
-        vector<int>sec;
-        k=k%n;
-        for(int i=n-k;i<n;i++){
-            sec.push_back(nums[i]);
-        }
-        for(int i=0;i<n-k;i++){
-            sec.push_back(nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            nums[i]=sec[i];
-        }
+        // int n=nums.size();
+        // vector<int>sec;
+        // k=k%n;
+        // for(int i=n-k;i<n;i++){
+        //     sec.push_back(nums[i]);
+        // }
+        // for(int i=0;i<n-k;i++){
+        //     sec.push_back(nums[i]);
+        // }
+        // for(int i=0;i<n;i++){
+        //     nums[i]=sec[i];
+        // }
+        int n = nums.size();
+        k = k % n; // Ensure k is within the range [0, n)
+
+        // Reverse the entire array
+        reverse(nums.begin(), nums.end());
+        
+        // Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        
+        // Reverse the rest of the elements after k
+        reverse(nums.begin() + k, nums.end());
         
     }
 };
