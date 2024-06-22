48d1391d209504fadaeca7daf1b982aa4d242594
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-22 14:43:32 +0530

    Time: 11 ms (81.57%) | Memory: 20.4 MB (31.13%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i;
-        for(i=0;i<nums.size();i++){
-            if(nums[i]!=i){
-                break;
-            }
+        // sort(nums.begin(),nums.end());
+        // int i;
+        // for(i=0;i<nums.size();i++){
+        //     if(nums[i]!=i){
+        //         break;
+        //     }
+        // }
+        // return i;   // TIME COMPLEXITY -> Nlog(N)+N
+        int N=nums.size();
+        int sum=N*(N+1)/2;
+        int sumo=0;
+        for(int i=0;i<N;i++){
+            sumo+=nums[i];
+
         }
-        return i;
+        return sum-sumo;
     }
 };
