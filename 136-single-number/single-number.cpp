feaa7b441c2c2da5640ee90cf5b0a96660bb304f
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-06-27 15:13:03 +0530

    Time: 32 ms (7.74%) | Memory: 23 MB (14.30%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        
+        map<int,int>m;
         for(int i=0;i<n;i++){
-            int num=nums[i];
-            int cnt=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    cnt++;
-                }
-                
-            }
-            if(cnt==1){
-                    return nums[i];
+            m[nums[i]]++;
+        }
+        for(auto it :m){
+            if(it.second==1){
+                return it.first;
             }
         }
         return -1;
