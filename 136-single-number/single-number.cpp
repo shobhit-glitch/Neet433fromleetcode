daa4751eafb6ca5577eb8b3def2ab905d14904f1
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-07-07 14:21:53 +0530

    Time: 7 ms (95.91%) | Memory: 19.3 MB (29.75%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,15 +2,27 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        map<int,int>m;
-        for(int i=0;i<n;i++){
-            m[nums[i]]++;
+        
+        // for(int i=0;i<n;i++){
+        //     int num=nums[i];
+        //     int cnt=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[i]==nums[j]){     TIME COMPLEXITY -> N^2
+        //             cnt++;
+        //         }
+                
+        //     }
+        //     if(cnt==1){
+        //             return nums[i];
+        //     }
+        // }
+        // return -1;
+        //ONE WITH ANOTHER VECTOR TO STORE OCCURENCE
+        //BIT MANIPULATION TECHNIQUE
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans=ans^nums[i];
         }
-        for(auto it :m){
-            if(it.second==1){
-                return it.first;
-            }
-        }
-        return -1;
+        return ans;
     }
 };
