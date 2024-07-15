7ea5599150d72cf62a5c88728904f18ee738339e
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-07-15 13:53:38 +0530

    Time: 5 ms (50.78%) | Memory: 9.3 MB (12.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int n=s.length();
-        string f;
+        
+        if(s==" "){
+            return true;
+        }
+        string str="";
         for(int i=0;i<n;i++){
             if(isalnum(s[i])){
-                f+=tolower(s[i]);
+                s[i]=tolower(s[i]);
+                str+=s[i];
             }
         }
-        int len=f.length();
         
-        bool flag=true;
-        for(int i=0;i<len/2;i++){
-            if(f[i]!=f[len-i-1]){
-                
+        //str=toLower(str);
+        cout<<str<<endl;
+        int m=str.length();
+        //if(m==1)return false;
+        for(int i=0;i<m/2;i++){
+            if(str[i]!=str[m-i-1]){
+                cout << str[i]<<i << " does not match " << str[m - i - 1] << endl;
                 return false;
-                break;
             }
         }
-        return flag;
+        return true;
     }
 };
