b7e21b940f2f204d5d06268fbbdb21023fe94922
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-07-20 04:30:53 +0530

    Time: 35 ms (92.04%) | Memory: 51.1 MB (22.15%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(maxi<i){
+                return false;
+            }
+            maxi=max(maxi,nums[i]+i);
+        }
+        return true;
+    }
+};
