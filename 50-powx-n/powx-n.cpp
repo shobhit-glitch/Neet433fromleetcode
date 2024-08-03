35a4789720a55f10cc82f013910a92c1b8145126
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-08-03 15:53:54 +0530

    Time: 4 ms (21.51%) | Memory: 8.3 MB (18.67%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,67 +1,22 @@
 class Solution {
 public:
-
     double myPow(double x, int n) {
-        //**************** [ BRUTE FORCE APPROACH ] ****************
-        // double ans=1;
-        // int f=abs(n);
-        // if(abs(x)==1){
-        //     if(x==-1){
-        //         return -1;
-        //     }
-            
-        //     return ans;
-        // }
-        // for(int i=0;i<f;i++){
-        //     ans*=x;
-        // }
-        // if(n<0){
-        //     if(n<-10000 && x!=1){
-        //         return 0;
-        //     }
-        //     else{
-        //         return 1/ans;
-        //     }
-        // }
-        // return ans;
-        //**************** [  OPTIMAL SOLUTION ] ****************
-        // double ans=1;
-        // long long f=abs(n);
-        // while(f>0){
-        //     if(f%2==0){
-        //         x=x*x;
-        //         f=f/2;
-        //     }
-        //     else{
-        //         ans=ans*x;
-        //         f=f-1;
-
-        //     }
-        // }
-        // if(n<0){
-        //     ans=1/ans;
-        // }
-        // return ans;
-        //**************** [  RECCURSIVE SOLUTION ] ****************
-        double res=1;
-        long long nn=n;
-        if(nn<0){
-            nn=-nn;
-        }
-        while(nn){
-            if(nn%2){
-                res=res*x;
-                nn=nn-1;
+        int m=abs(n);
+        double ans=1;
+        while(m>0){
+            if(m%2==0){
+                x=x*x;
+                m=m/2;
             }
             else{
-                x=x*x;
-                nn=nn/2;
+                ans=ans*x;
+                m=m-1;
             }
         }
         if(n<0){
-            res=(double)(1) /(double)(res);
-            
+            ans=1/ans;
+
         }
-        return res;
+        return ans;
     }
 };
