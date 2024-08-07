e4b00bd301aa1f68853267c2977e60681affa875
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-08-08 05:26:57 +0530

    Time: 5 ms (24.97%) | Memory: 12.3 MB (19.69%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
+bool func(vector<int> vec,int target,int n){
+    int l=0;
+    int r=n-1;
+    if(vec[r]<target){
+        return false;
+    }
+    if(vec[l]>target){
+        return false;
+    }
+    while(l<=r){
+        int mid=l+(r-l)/2;
+        if(vec[mid]==target){
+            return true;
+        }
+        if(vec[mid]>target){
+            r=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    return false;
+}
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
+        int m=matrix[0].size();
         for(int i=0;i<n;i++){
-            int m=matrix[i].size();
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-                cout<<matrix[i][j]<<" ";
+            if(func(matrix[i],target,m)){
+                return true;
             }
-            cout<<endl;
         }
         return false;
     }
