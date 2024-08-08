2ea2ad5b4fe20d70a0a571bcb7a6dad44ade845b
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-08-08 06:00:32 +0530

    Time: 3 ms (66.35%) | Memory: 12.2 MB (19.69%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
-bool func(vector<int> vec,int target,int n){
-    int l=0;
-    int r=n-1;
-    if(vec[r]<target){
-        return false;
-    }
-    if(vec[l]>target){
-        return false;
-    }
-    while(l<=r){
-        int mid=l+(r-l)/2;
-        if(vec[mid]==target){
-            return true;
-        }
-        if(vec[mid]>target){
-            r=mid-1;
-        }
-        else{
-            l=mid+1;
-        }
-    }
-    return false;
-}
+// bool func(vector<int> vec,int target,int n){
+//     int l=0;
+//     int r=n-1;
+//     if(vec[r]<target){
+//         return false;
+//     }
+//     if(vec[l]>target){
+//         return false;
+//     }
+//     while(l<=r){
+//         int mid=l+(r-l)/2;
+//         if(vec[mid]==target){
+//             return true;
+//         }
+//         if(vec[mid]>target){
+//             r=mid-1;
+//         }
+//         else{
+//             l=mid+1;
+//         }
+//     }
+//     return false;
+// }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        for(int i=0;i<n;i++){
-            if(func(matrix[i],target,m)){
+        // for(int i=0;i<n;i++){
+        //     if(func(matrix[i],target,m)){
+        //         return true;
+        //     }
+        // }
+        //return false;
+        int row=0;
+        int col=m-1;
+        while(row<n && col>=0){
+            if(target==matrix[row][col]){
                 return true;
             }
+            if(target>matrix[row][col]){
+                row++;
+            }
+            else{
+                col--;
+            }
         }
-        return false;
+    return false;
+
     }
 };
