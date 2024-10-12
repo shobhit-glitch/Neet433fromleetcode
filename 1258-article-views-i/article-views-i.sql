ba3ce3a62db3cbf229087102930dbcf9ae7a8b79
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-10-12 15:06:43 +0530

    Time: 399 ms (61.05%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select author_id as id
-from Views
-where viewer_id=author_id 
-group by id 
-order by id asc;
+from views 
+where viewer_id=author_id
+group by author_id
+order by author_id asc;
