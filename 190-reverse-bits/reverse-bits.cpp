10bb2f35aa2020fb7a289fe8205062f79e003048
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-10-23 15:26:06 +0530

    Time: 5 ms (7.50%) | Memory: 7.4 MB (16.44%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t reversed=0;
-        for(int i=0;i<32;i++){
-            reversed|=((n>>i)&1)<<(31-i);
+        // uint32_t reversed=0;
+        // for(int i=0;i<32;i++){
+        //     reversed|=((n>>i)&1)<<(31-i);
+        // }
+        // return reversed;
+        for(int i=0;i<16;i++){
+            uint32_t lft=(n>>(31-i)&1);
+            uint32_t ryt=(n>>i)&1;
+            if(lft!=ryt){
+                n ^= (1 << (31 - i));
+            
+            n ^= (1 << i);
+            }
         }
-        return reversed;
+        return n;
     }
 };
