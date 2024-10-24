f7a0aa172b4e0b45550af29bdeff0d7aa5cc17d1
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2024-10-24 10:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (51.34%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //TC->O(N)
-        // int n=nums.size();
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==target){
-        //         return i;
-        //     }
-        // }
-        // return -1;
-        // CAN BE DONE IN TC->O(nlog(n))
-        //BINARY SEARCH 
         int l=0;
-        int r=nums.size()-1;
-        int ans=-1;
+        int n=nums.size();
+        int r=n-1;
+        int pos=-1;
         while(l<=r){
             int mid=l+(r-l)/2;
             if(nums[mid]==target){
-                ans=mid;
+                pos=mid;
+                return pos;
             }
             if(nums[l]<=nums[mid]){
                 if(nums[l]<=target && target<=nums[mid]){
                     r=mid-1;
                 }
                 else{
-                l=mid+1;
-            }
+                    l=mid+1;
+                }
             }
             else{
                 if(nums[mid]<=target && nums[r]>=target){
@@ -36,6 +28,6 @@ public:
                 }
             }
         }
-        return ans;
+        return pos;
     }
 };
