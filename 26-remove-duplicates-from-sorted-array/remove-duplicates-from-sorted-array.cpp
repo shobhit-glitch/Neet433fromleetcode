3377fd920f186d728a847fc82d59938b2a848553
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2025-01-24 01:28:02 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (49.11%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n=nums.size();
-        // 2 POINTER APPROACH  O(N)
         int i=1;
+        int n=nums.size();
         for(int j=1;j<n;j++){
             if(nums[j]!=nums[j-1]){
                 nums[i]=nums[j];
@@ -12,6 +11,4 @@ public:
         }
         return i;
     }
-
-    
 };
