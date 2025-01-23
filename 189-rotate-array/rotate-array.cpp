4d052a0d38cccf21bf2f85d6ebfcc863c0b67722
shobhit-glitch <99634491+shobhit-glitch@users.noreply.github.com>
2025-01-24 02:03:23 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (78.24%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
-
     void rotate(vector<int>& nums, int k) {
-        
-        // int n=nums.size();
-        // vector<int>sec;
-        // k=k%n;
-        // for(int i=n-k;i<n;i++){
-        //     sec.push_back(nums[i]);
-        // }
-        // for(int i=0;i<n-k;i++){
-        //     sec.push_back(nums[i]);
-        // }
-        // for(int i=0;i<n;i++){
-        //     nums[i]=sec[i];
-        // }
-        int n = nums.size();
-        k = k % n; 
+        int n=nums.size();
+        k=k%n;
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
         reverse(nums.begin() + k, nums.end());
-        
     }
 };
